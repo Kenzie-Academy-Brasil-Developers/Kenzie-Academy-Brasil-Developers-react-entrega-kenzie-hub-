7748348bfd0e3cdd966d496921b10c007bfd1170
Julyanna Gama <julyannagama6@gmail.com>
2024-02-29 17:34:53 -0300

    correção

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -12,9 +12,9 @@ export const Dashboard = ({ setIsDashboard, user}) => {
     <div className={styles.dashboardContainer}>
       <div className={styles.dashboardContent}>
         <div className={styles.welcomeMessage}>
-          <h3 className={styles.title}>Olá, {user.name}</h3>
+          <h3 className={styles.title}>Olá, {user?.name}</h3>
           <p className={styles.moduleInfo}>
-          {user.course_module}
+          {user?.course_module}
           </p>
         </div>
       </div>
