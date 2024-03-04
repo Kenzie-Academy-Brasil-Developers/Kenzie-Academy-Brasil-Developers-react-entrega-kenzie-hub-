7f4ebe6fe9385ca46f8175fdb5f2a317c33f52ab
Julyanna Gama <julyannagama6@gmail.com>
2024-03-04 14:53:21 -0300

    Correção

--- src/pages/Login/LoginForm/index.jsx
+++ src/pages/Login/LoginForm/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./styles.module.scss";
 import { useForm } from "react-hook-form";
-import { LoginSchema} from "./LoginSchema";
+import { LoginSchema } from "./LoginSchema";
 import { ToastContainer, toast, Bounce } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Input } from "../../../components/forms/Input";
@@ -9,7 +9,7 @@ import { userApi } from "../../../services/api";
 import { useContext } from "react";
 import { ExampleContext } from "../../provides/context";
 import { zodResolver } from "@hookform/resolvers/zod";
- 
+
 export const LoginForm = () => {
   const navigate = useNavigate();
   const { setUser } = useContext(ExampleContext);
@@ -24,7 +24,6 @@ export const LoginForm = () => {
   const postUsers = async (formData) => {
     try {
       const { data } = await userApi.post("/sessions", formData);
-      console.log(data);
       setUser(data.user);
       localStorage.setItem("@TOKEN", data.token);
       navigate("/dashboard");
@@ -45,14 +44,14 @@ export const LoginForm = () => {
           {...register("email")}
           placeholder="Digite seu email..."
         />
-          <p className={styles.paragraph}>{errors.email?.message}</p>
+        <p className={styles.paragraph}>{errors.email?.message}</p>
         <Input
           label="Senha"
           type="password"
           {...register("password")}
           placeholder="Digite sua senha..."
         />
-         <p className={styles.paragraph}>{errors.password?.message}</p>
+        <p className={styles.paragraph}>{errors.password?.message}</p>
         <button type="submit" className={styles.buttonLogin}>
           Entrar
         </button>
